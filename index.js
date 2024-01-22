5a78190485d945aa132d117618795b3b8e07d1c2
Adarsh Tiwari <tadarsh249@gmail.com>
2024-01-22 16:08:08 +0530

    bug fixes

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
 const path = require("path");
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const https = require("https");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,7 +18,7 @@ app.get("/", (req, res) => {
 });
 app.post("/", (req, res) => {
   let city = req.body.cityname;
-//   console.log(city);
+  //   console.log(city);
   let api_key = process.env.API_KEY;
   let url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -27,24 +26,22 @@ app.post("/", (req, res) => {
     "&appid=" +
     api_key +
     "&units=metric";
-    // console.log(url);
+  // console.log(url);
   https.get(url, (response) => {
     response.on("data", (data) => {
-            console.log('hello');
-            let weatherdata = JSON.parse(data);
-            let temp = weatherdata.main.temp;
-            let desc = weatherdata.weather[0].description;
-            let icon = weatherdata.weather[0].icon;
-            let img_src =
-              "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-            const weather_info = {
-              cityname: city,
-              temperature: temp,
-              description: desc,
-              img_src: img_src,
-            };
-            res.status(200).render("weather", weather_info);
-      
+      console.log("hello");
+      let weatherdata = JSON.parse(data);
+      let temp = weatherdata.main.temp;
+      let desc = weatherdata.weather[0].description;
+      let icon = weatherdata.weather[0].icon;
+      let img_src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      const weather_info = {
+        cityname: city,
+        temperature: temp,
+        description: desc,
+        img_src: img_src,
+      };
+      res.status(200).render("weather", weather_info);
     });
   });
 });
